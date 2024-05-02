7491502b2af0a6e6af6ab74f475e31dd4310184e
Fidan-20 <shamilovafidan@gmail.com>
2024-05-02 05:44:41 -0700

    kub(zer)

---
+++ kub(zer)/kub/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Dice from './Dice';
+import './App.css'
+
+const App = () => {
+  const [dice1, setDice1] = useState(1);
+  const [dice2, setDice2] = useState(1);
+
+  const rollDice = () => {
+    setDice1(Math.floor(Math.random() * 6) + 1);
+    setDice2(Math.floor(Math.random() * 6) + 1);
+  };
+
+  return (
+    <div className="app">
+      <h1>Dice Roller</h1>
+      <div className="dice-container">
+        <Dice number={dice1} />
+        <Dice number={dice2} />
+      </div>
+      <button onClick={rollDice}>Roll Dice</button>
+    </div>
+  );
+};
+
+export default App;
